8a7da29b378a66d6d40717e623e96060f56d9598
Prateekagarwal9 <39221904+Prateekagarwal9@users.noreply.github.com>
2021-03-19 16:57:18 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const http = require('http');
+function function2() {
+    // all the stuff you want to happen after that pause
+    console.log('Testing');
+}
+
 
 const server = http.createServer((request, response) => {
+    setTimeout(function2, 3000);
     response.writeHead(200, {"Content-Type": "text/plain"});
     response.end("Hello World!");
 });
